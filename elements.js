94d26e78a47c64f73f5e91620dcf9e02be106e22
Sundeep Narang <sundeepnrng@gmail.com>
2018-11-08 20:45:44 -0500

    Update elements.js

--- elements.js
+++ elements.js
@@ -4,5 +4,14 @@ module.exports = {
     "Monsters" : ["Trent", "Dragon", "Vampire", "Ghost", "Golem", "Phoenix", "Thunderbird"],
     "Air" : ["Air", "Ash", "Dust", "Snow", "Steam", " Storm"],
     "Plant" : ["Coffee", "Fern", "Grass", "PalmTree", "Reed", "Tree"],
-    "Germ"  : ["Bacteria", "Scorpion", "Plankton", "Beetle", "Shells", "Weeds", "Worm", ""]
+    "Germ"  : ["Bacteria", "Scorpion", "Plankton", "Beetle", "Shells", "Weeds", "Worm", "Mushroom", "Moss", "Butterfly"],
+    "Animals" : ["Fish", "Beast", "Lizard", "Bird", "Snake", "Dinosaur", "Turtle", "Dolphin", "Whale", "DomesticAnimal"],
+    "FoodEtc" : ["Dough","Feather", "Wheat", "Wool", "Flour", "Meat", "Milk"],
+    "Earth" : ["Clay", "Metal", "Earth", "Sand", "Fertilizer", "Stone", "Glass", "Limestone"],
+    "Water" : ["Ice", "Quicksilver", "Swamp" ,"Water"],
+    "Human" : ["Assasin", "Corpse", "Human", "Hunter", "Warrior", "Wizard"],
+    "Vehicles" : ["Boat", "Ship", "Steamship"],
+    "BuildMat" : ["Bricks". "Cement", "Ceramics", "Clothing", "Concrete", "Fabric", "Field", " Wood"],
+    "ToolsHouses" : ["Boiler", "Tools", "SteamEngine", "Weapon", "House", "Hut", "PoisonedWeapon", "Skyscraper"]
+    
 };
