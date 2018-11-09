d5e0ef50bdd34e98b6239f951e5075fca06717d3
sundeepnarang <sundeepnrng@gmail.com>
2018-11-08 19:57:48 -0500

    started

---
+++ elements.js
@@ -0,0 +1,8 @@
+module.exports = {
+    "Life" : ["Blood", "Egg", "Electricity", "Energy", "Life", "Poison", "Seeds", "Sex", "Void"],
+    "Fire" : ["Coal", "Sulfur", "Fire", "Lava", " Oil", "Plasma"],
+    "Monsters" : ["Trent", "Dragon", "Vampire", "Ghost", "Golem", "Phoenix", "Thunderbird"],
+    "Air" : ["Air", "Ash", "Dust", "Snow", "Steam", " Storm"],
+    "Plant" : ["Coffee", "Fern", "Grass", "PalmTree", "Reed", "Tree"],
+    "Germ"  : ["Bacteria", "Scorpion", "Plankton", "Beetle", "Shells", "Weeds", "Worm", ""]
+};
